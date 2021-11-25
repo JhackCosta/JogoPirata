48563fde87071564268c5b3aa859f100084bfd42
JhackCosta <jhacktibia@gmail.com>
2021-11-25 19:45:19 -0300

    segundo commit

--- JavaScript/regras.js
+++ JavaScript/regras.js
@@ -1,4 +1,6 @@
-function pergunta(pergunta, resposta, proxima) {
+///////////////////////////////////// sem oportunidades ////////////////////////////
+
+/*function pergunta(pergunta, resposta, proxima) {
 
     var lerResposta = prompt(pergunta)
 
@@ -14,6 +16,31 @@ function pergunta(pergunta, resposta, proxima) {
         alert("A resposta incorreta você foi jogado ao mar!\n A reposta Correta é: " + respostaCorreta)
         window.location.href = "../pirata/gameOver.html"
 
+    }
+
+}*/
+////////////////////////////////// 3 oportunidades///////////////////
+function pergunta(pergunta, resposta, proxima) {
+
+    var lerResposta = prompt(pergunta)
+
+    var respostaCorreta = resposta.toLocaleUpperCase()
+    var verificarResposta = lerResposta.toLocaleUpperCase()
+
+    var i = 0
+    while (i < 3) {
+        if (verificarResposta === respostaCorreta) {
+
+            alert("Parabens marujo! você é genial... ")
+            window.location.href = proxima
+            break;
+        } else {
+            alert("A resposta incorreta você foi jogado ao mar!")
+            lerResposta = prompt(pergunta)
+
+        }
+        window.location.href = "../pirata/gameOver.html"
+        i++
     }
 
 }
